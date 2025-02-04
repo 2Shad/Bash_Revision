89ac557148831309ff12bfaa427293068f0f0923
2Shad <shadman.ahmed098@gmail.com>
2025-02-04 16:20:29 +0000

    intermediate challanges

---
+++ Scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# # Intermediate Challanges
+# echo "Type your name: "
+# read name
+# echo "Hello $name"
+
+
+# for file in txt_files/*.txt; do
+#     echo "Reading file: $file"
+# done
+
